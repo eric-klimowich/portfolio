f0c758c5dd3adc64280dd31ce3a2e086759ba6aa
eric-klimowich <eklimowich@hotmail.com>
2019-03-18 17:36:31 -0400

    Addressed all alerts concerning links, alt, and className, and links open in new page

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -3,21 +3,23 @@ import React from 'react'
 const Links = () => {
   return (
     <div>
-      <div>
+      <div id="my-name" >
         Eric Klimowich
       </div>
-      <a target="_blank" href="https://github.com/eric-klimowich" >
+      <div>
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/eric-klimowich" >
         <i className="ui github icon" ></i>
       </a>
-      <a target="_blank" href="https://www.linkedin.com/in/eric-klimowich-523a8a56/" >
+      <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/eric-klimowich-523a8a56/" >
         <i className="ui linkedin icon" ></i>
       </a>
-      <a target="_blank" href="https://medium.com/@eric.klimowich" >
+      <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@eric.klimowich" >
         <i className="ui medium icon" ></i>
       </a>
-      <a target="_blank" href="mailto:eric.klimowich@gmail.com" >
+      <a target="_blank" rel="noopener noreferrer" href="mailto:eric.klimowich@gmail.com" >
         <i className="ui envelope icon" ></i>
       </a>
+      </div>
     </div>
   )
 }
