7919b7a69912f202f0c42fa4fb9ac0fd029853f6
eric-klimowich <eklimowich@hotmail.com>
2019-03-28 14:49:13 -0400

    Fetched my blog entries from Medium to post on Blog page

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Blog = () => {
-  return (
-    <div>
-      This app is about my blog.
-    </div>
-  )
+class Blog extends Component {
+
+  state = {
+    blogs: []
+  }
+
+  componentDidMount() {
+    fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@eric.klimowich')
+      .then(res => res.json())
+      .then(blogs => this.setState({ blogs: blogs.items }))
+  }
+
+  render() {
+    console.log(this.state.blogs)
+    return (
+      <div>
+        This app is about my blog.
+      </div>
+    )
+  }
 }
 
 export default Blog
