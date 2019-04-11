9901375d56d33dd447fc3496b80ce0b835f32e39
eric-klimowich <eklimowich@hotmail.com>
2019-04-11 16:13:34 -0400

    Added BlogCategory to render each category after they are iterated over in BlogEntry

---
+++ src/pages/BlogCategory.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlogCategory = props => {
+  console.log(props)
+  return (
+    <div>
+      {props.category}
+    </div>
+  )
+}
+
+export default BlogCategory
--- src/pages/BlogEntry.js
+++ src/pages/BlogEntry.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import BlogCategory from './BlogCategory'
+
 const BlogEntry = props => {
   console.log(props)
   return (
     <div>
       {props.entry.title}
+      {props.entry.categories.map(category => <BlogCategory category={category} />)}
     </div>
   )
 }
