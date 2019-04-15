8b3764bd1bb912862cf30dd38264f6c86cacf2b2
eric-klimowich <eklimowich@hotmail.com>
2019-04-15 07:00:49 -0400

    Used uuid to create keys for categories in each blog post

--- src/pages/BlogCategory.js
+++ src/pages/BlogCategory.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const BlogCategory = props => {
-  console.log(props)
+  // console.log(props)
   return (
     <div>
       {props.category}
--- src/pages/BlogEntry.js
+++ src/pages/BlogEntry.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import uuid from 'uuid'
 
 import BlogCategory from './BlogCategory'
 
 const BlogEntry = props => {
-  console.log(props.entry.pubDate)
+  // console.log(props)
   return (
     <div>
       {props.entry.title}
-      {props.entry.categories.map(category => <BlogCategory category={category} />)}
+      {props.entry.categories.map(category => <BlogCategory key={uuid.v4()} category={category} />)}
       {props.entry.description.slice(0, 110).replace(/<p>|<h3>|<\/p>|<\/h3>/g, '')}
       <a target="_blank" rel="noopener noreferrer" href={props.entry.link}>
         (more...)
