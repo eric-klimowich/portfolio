804cfd2050ee86d35f977f62b431f7d8d4a59783
eric-klimowich <eklimowich@hotmail.com>
2019-04-15 08:19:35 -0400

    Added custom styling to Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,25 +4,25 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">
-                Home
-              </Link>
-            </li>
-            <li>
+      <header className="main-header">
+        <div className="main-header__title">
+          <Link to="/">
+            Eric Klimowich
+          </Link>
+        </div>
+        <nav className="main-nav">
+          <ul className="main-nav__items">
+            <li className="main-nav__item">
               <Link to="/about">
                 About
               </Link>
             </li>
-            <li>
+            <li className="main-nav__item">
               <Link to="/projects">
                 Projects
               </Link>
             </li>
-            <li>
+            <li className="main-nav__item">
               <Link to="/blog">
                 Blog
               </Link>
