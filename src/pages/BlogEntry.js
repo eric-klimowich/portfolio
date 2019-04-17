ec9f2101c307b9737705857819ce1b6739a8c6b3
eric-klimowich <eklimowich@hotmail.com>
2019-04-17 07:46:05 -0400

    Added additional structure to links in Projects for styling purposes and consistency, added new styling to Projects

--- src/pages/BlogEntry.js
+++ src/pages/BlogEntry.js
@@ -8,16 +8,16 @@ const BlogEntry = props => {
   return (
     <div className="blog">
       <h1 className="blog-title">{props.entry.title}</h1>
-      <div className="blog-categories-container">
-        <ul className="blog-categories">
-          {props.entry.categories.map(category => <BlogCategory key={uuid.v4()} category={category} />)}
-        </ul>
-      </div>
       <div className="blog-description">
         {props.entry.description.slice(0, 200).replace(/<.*>/g, '')}
         <a target="_blank" rel="noopener noreferrer" href={props.entry.link}>
           (more...)
         </a>
+      </div>
+      <div className="blog-categories-container">
+        <ul className="blog-categories">
+          {props.entry.categories.map(category => <BlogCategory key={uuid.v4()} category={category} />)}
+        </ul>
       </div>
     </div>
   )
