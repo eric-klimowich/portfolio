5905fc8d681b021eae4295764b8492d401ac53fb
eric-klimowich <eklimowich@hotmail.com>
2019-04-16 22:43:35 -0400

    Reconfigured JSX of About page to accomodate styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const About = () => {
   return (
-    <div>
-      <img className="profile-image" src="/images/profile_picture.jpg" alt="Eric's Profile" />
-      <p>
-        Hi, I'm Eric. I'm a full stack developer with a passion for creating stuff.
-      </p>
-      <p>
-        Prior to programming, I spent 16 years in the not-for-profit world and a year in education.
-      </p>
-      <p>
-        All of these roles were people focused and I've brought that people-first mentality with me to the tech world. Overlap between program management, teaching, planning, and stuff.
-      </p>
-      <p>
-        When I'm not coding, I love going to sporting events, running, other stuff. Otherwise, I'm probably snuggling with my cats, Jack, Walter, Penny, and Olivia.
-      </p>
-    </div>
+    <Fragment>
+      <div>
+        <img
+          className="profile-image"
+          src="/images/profile_picture.jpg"
+          alt="Eric's Profile"
+        />
+      </div>
+      <div>
+        <p>
+          Hi, I'm Eric. I'm a full stack developer with a passion for creating stuff.
+        </p>
+        <p>
+          Prior to programming, I spent 16 years in the not-for-profit world and a year in education.
+        </p>
+        <p>
+          All of these roles were people focused and I've brought that people-first mentality with me to the tech world. Overlap between program management, teaching, planning, and stuff.
+        </p>
+        <p>
+          When I'm not coding, I love going to sporting events, running, other stuff. Otherwise, I'm probably snuggling with my cats, Jack, Walter, Penny, and Olivia.
+        </p>
+      </div>
+    </Fragment>
   )
 }
 
