293a72fb393a703951dd644f76c2705f78df773c
eric-klimowich <eklimowich@hotmail.com>
2019-04-16 22:40:23 -0400

    Completed styling of categories for blogs, removed icon and semantic styling links, portfolio is now 100% custom styling, added styling to image

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <img className="ui medium circular image" src="/images/profile_picture.jpg" alt="Eric's Profile" />
+      <img className="profile-image" src="/images/profile_picture.jpg" alt="Eric's Profile" />
       <p>
         Hi, I'm Eric. I'm a full stack developer with a passion for creating stuff.
       </p>
--- src/pages/Links.js
+++ src/pages/Links.js
@@ -4,16 +4,16 @@ const Links = () => {
   return (
     <div>
       <a target="_blank" rel="noopener noreferrer" href="https://github.com/eric-klimowich">
-        <i className="ui github icon"></i>
+        Github
       </a>
       <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/eric-klimowich-523a8a56/">
-        <i className="ui linkedin icon"></i>
+        LinkedIn
       </a>
       <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@eric.klimowich">
-        <i className="ui medium icon"></i>
+        Blog
       </a>
       <a target="_blank" rel="noopener noreferrer" href="mailto:eric.klimowich@gmail.com">
-        <i className="ui envelope icon"></i>
+        Email
       </a>
     </div>
   )
