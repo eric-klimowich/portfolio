379d00c66b2068478b8cc7ecf5648e582faea6b5
eric-klimowich <eklimowich@hotmail.com>
2019-04-16 21:58:24 -0400

    Added boxes for blog entries

--- src/pages/BlogEntry.js
+++ src/pages/BlogEntry.js
@@ -6,14 +6,14 @@ import BlogCategory from './BlogCategory'
 const BlogEntry = props => {
   // console.log(props)
   return (
-    <div>
-      <h1>{props.entry.title}</h1>
+    <div className="blog">
+      <h1 className="blog-title">{props.entry.title}</h1>
       <div className="blog-categories-container">
         <ul className="blog-categories">
           {props.entry.categories.map(category => <BlogCategory key={uuid.v4()} category={category} />)}
         </ul>
       </div>
-      <div className="blog-descriptions">
+      <div className="blog-description">
         {props.entry.description.slice(0, 200).replace(/<.*>/g, '')}
         <a target="_blank" rel="noopener noreferrer" href={props.entry.link}>
           (more...)
