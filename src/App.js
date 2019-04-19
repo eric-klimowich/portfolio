1bb6f1059a6997107c12f69f64db18c3b3ea847c
eric-klimowich <eklimowich@hotmail.com>
2019-04-19 11:50:29 -0400

    Added Home page to portfolio, added Footer to Home, rendered Home page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
-import Links from './pages/Links'
+import Home from './pages/Home'
 import About from './pages/About'
 import Projects from './pages/Projects'
 import Blog from './pages/Blog'
@@ -15,10 +15,10 @@ class App extends Component {
         <BrowserRouter>
           <div>
             <Navbar />
-            <Route path="/" exact component={Links} />
+            <Route path="/" exact component={Home} />
             <Route path="/about" exact component={About} />
             <Route path="/projects" exact component={Projects} />
-            <Route path="/blog" exact component={Blog} />
+            <Route path="/blogs" exact component={Blog} />
           </div>
         </BrowserRouter>
       </div>
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <Fragment>
+      <h1>This website is about me!</h1>
+      <Footer />
+    </Fragment>
+  )
+}
+
+export default Home
