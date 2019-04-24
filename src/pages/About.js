155b1e97cbf97bf8cb8fc2a3530e7b80e2052f2b
eric-klimowich <eklimowich@hotmail.com>
2019-04-24 14:25:41 -0400

    Added additional photos for About page, new layout begun for About page with descriptions and pictures

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,37 +5,56 @@ import Footer from '../components/Footer'
 const About = () => {
   return (
     <Fragment>
-      <div className="image">
-        <img
-          className="profile-image"
-          src="/images/pictures/profile_picture.jpg"
-          alt="Eric's Profile"
-        />
+      <div className="blurb">
+        <div className="blurb__info">
+          <h2 className="blurb__title">
+            Full Stack Web Developer
+          </h2>
+          <p className="blurb__text">
+            Hi, I'm Eric. I'm a full stack developer with a passion for education and working with communities. Utilizing my experience in Ruby on Rails, JavaScript, and React, I have built applications that streamline file sharing for teachers and track consumers’ usages. My background includes serving as a general education teacher and intervention specialist, as well as having a proven ability to manage and develop programs, staff, and budgets, effectively increasing revenue of non-profit organizations. As a developer, I hope to contribute to companies that invest in their communities, foster a diverse environment, and maintain a healthy fiscal and social approach.
+          </p>
+        </div>
+        <div className="blurb__image--container">
+          <img className="image" src="/images/pictures/atComputer.jpg"/>
+        </div>
       </div>
-      <div className="bio">
-        <p>
-          Hi, I'm Eric. I'm a full stack developer with a passion for creating stuff.
-        </p>
-        <p>
-          Prior to programming, I spent 16 years in the not-for-profit world and a year in education.
-        </p>
-        <p>
-          All of these roles were people focused and I've brought that people-first mentality with me to the tech world. Overlap between program management, teaching, planning, and stuff.
-        </p>
-        <p>
-          When I'm not coding, I love going to sporting events, running, other stuff. Otherwise, I'm probably snuggling with my cats, Jack, Walter, Penny, and Olivia.
-        </p>
+      <div className="blurb">
+      <div className="blurb__image--container">
+        <img className="image" src="/images/pictures/teacher.jpg"/>
+      </div>
+        <div className="blurb__info">
+          <h2 className="blurb__title">
+            Teacher
+          </h2>
+          <p className="blurb__text">
+            Prior to becoming a developer, I was an elementary school teacher for the School District of Osceola County and NYC Department of Education.
+          </p>
+        </div>
       </div>
       <div className="blurb">
         <div className="blurb__info">
           <h2 className="blurb__title">
-            Blurb Title
+            Aquatics Director
           </h2>
-          <p>
-            Blah blah blah blah blah.
+          <p className="blurb__text">
+            Aquatics Director, YMCA.
           </p>
         </div>
         <div className="blurb__image--container">
+          <img className="image" src="/images/pictures/aqDir.jpg"/>
+        </div>
+      </div>
+      <div className="blurb">
+        <div className="blurb__image--container">
+          <img className="image" src="/images/pictures/running.jpg"/>
+        </div>
+        <div className="blurb__info">
+          <h2 className="blurb__title">
+            Running
+          </h2>
+          <p className="blurb__text">
+            Running.
+          </p>
         </div>
       </div>
     <Footer />
