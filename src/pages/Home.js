0222f6745b87305faf35baee3901364330005a5b
eric-klimowich <eklimowich@hotmail.com>
2019-04-25 12:04:50 -0400

    Added profile picture and text to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,19 @@ const Home = () => {
   return (
     <Fragment>
       <h1>This website is about me!</h1>
+      <div className="blurb">
+        <div className="blurb__image--container">
+          <img className="image" src="images/pictures/profile_picture.jpg"/>
+        </div>
+        <div className="blurb__info">
+          <h1 className="blurb__title">
+            Hi!
+          </h1>
+          <p className="blurb__text">
+            Welcome to my website, the place to go for all things Eric Klimowich.
+          </p>
+        </div>
+      </div>
       <Footer />
     </Fragment>
   )
