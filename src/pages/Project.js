407f3ed903384124fc67300ad716aee6e057020d
eric-klimowich <eklimowich@hotmail.com>
2019-05-01 13:58:32 -0400

    Added container <div>'s to Project component for styling

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -10,23 +10,27 @@ const Project = props => {
       <div className="project__screenshot-container">
         <img className="project__screenshot" src={props.screenshot} />
       </div>
-      <p className="project__description">
-        {props.description}
-      </p>
-      <ul className="project__links">
-        {props.githubLink ? Functions.renderSingleGitRepo(props) : Functions.renderFrontAndBackGitRepo(props)}
-        <li className="project__link-container">
-          <a
-            className="project__link"
-            target="_blank"
-            rel="noopener noreferrer"
-            href={props.demoLink}
-          >
-          <img className="youtube-image" src="/images/logos/yt_icon_rgb.png" alt=""/>
-            Demo
-          </a>
-        </li>
-      </ul>
+      <div className="project__description-container">
+        <p className="project__description">
+          {props.description}
+        </p>
+      </div>
+      <div className="project__links-container">
+        <ul className="project__links">
+          {props.githubLink ? Functions.renderSingleGitRepo(props) : Functions.renderFrontAndBackGitRepo(props)}
+          <li className="project__link-container">
+            <a
+              className="project__link"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={props.demoLink}
+            >
+            <img className="youtube-image" src="/images/logos/yt_icon_rgb.png" alt=""/>
+              Demo
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
