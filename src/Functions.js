901c7da27a48bf88b08c537ef00cc52f7cc50c71
eric-klimowich <eklimowich@hotmail.com>
2019-06-02 08:17:12 -0400

    Added functions to render both Navs, conditionally render both Navs, need to fix onClick

--- src/Functions.js
+++ src/Functions.js
@@ -1,4 +1,6 @@
 import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
+import MobileNav from './components/MobileNav'
 
 export const renderFrontAndBackGitRepo = props => {
   return (
@@ -118,5 +120,95 @@ export const renderBlurbPicBoth = props => {
         <img className="blurb__image" src={props.blurbImage2}/>
       </div>
     </div>
+  )
+}
+
+export const renderNavbar = handleShowMobileNav => {
+  return (
+    <header className="main-header" onClick={handleShowMobileNav}>
+      <button class="toggle-button">
+          <span class="toggle-button__bar"></span>
+          <span class="toggle-button__bar"></span>
+          <span class="toggle-button__bar"></span>
+      </button>
+      <div className="main-header__title">
+        <Link to="/">
+          Eric Klimowich
+        </Link>
+      </div>
+      <nav className="main-nav">
+        <ul className="main-nav__items">
+          <li className="main-nav__item">
+            <Link to="/about">
+              About
+            </Link>
+          </li>
+          <li className="main-nav__item">
+            <Link to="/projects">
+              Projects
+            </Link>
+          </li>
+          <li className="main-nav__item">
+            <Link to="/blogs">
+              Blogs
+            </Link>
+          </li>
+          <li className="main-nav__item main-nav__item--cta">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://drive.google.com/open?id=0B7jl9f0rvdyPeWZrYk1Zb3RmZDh1Sm0zYWZiYl9PZU1RUDVJ"
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+export const renderMobileNav = handleShowMobileNav => {
+  return (
+    <header className="main-header" onClick={handleShowMobileNav}>
+      <button class="toggle-button">
+          <span class="toggle-button__bar"></span>
+          <span class="toggle-button__bar"></span>
+          <span class="toggle-button__bar"></span>
+      </button>
+      <MobileNav />
+      <div className="main-header__title">
+        <Link to="/">
+          Eric Klimowich
+        </Link>
+      </div>
+      <nav className="main-nav">
+        <ul className="main-nav__items">
+          <li className="main-nav__item">
+            <Link to="/about">
+              About
+            </Link>
+          </li>
+          <li className="main-nav__item">
+            <Link to="/projects">
+              Projects
+            </Link>
+          </li>
+          <li className="main-nav__item">
+            <Link to="/blogs">
+              Blogs
+            </Link>
+          </li>
+          <li className="main-nav__item main-nav__item--cta">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://drive.google.com/open?id=0B7jl9f0rvdyPeWZrYk1Zb3RmZDh1Sm0zYWZiYl9PZU1RUDVJ"
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,62 +1,24 @@
 import React, { Component } from 'react'
 import MobileNav from './MobileNav'
 import { Link } from 'react-router-dom'
+import { renderNavbar } from '../Functions'
+import { renderMobileNav } from '../Functions'
 
 class Navbar extends Component {
 
   state = {
-    clicked: false
+    showMobileNav: false
   }
 
   handleShowMobileNav = () => {
     this.setState({
-      clicked: !this.state.clicked
+      showMobileNav: !this.state.showMobileNav
     })
   }
 
   render() {
     return (
-      <header className="main-header" onClick={this.handleShowMobileNav}>
-        <button class="toggle-button">
-            <span class="toggle-button__bar"></span>
-            <span class="toggle-button__bar"></span>
-            <span class="toggle-button__bar"></span>
-        </button>
-        <MobileNav />
-        <div className="main-header__title">
-          <Link to="/">
-            Eric Klimowich
-          </Link>
-        </div>
-        <nav className="main-nav">
-          <ul className="main-nav__items">
-            <li className="main-nav__item">
-              <Link to="/about">
-                About
-              </Link>
-            </li>
-            <li className="main-nav__item">
-              <Link to="/projects">
-                Projects
-              </Link>
-            </li>
-            <li className="main-nav__item">
-              <Link to="/blogs">
-                Blogs
-              </Link>
-            </li>
-            <li className="main-nav__item main-nav__item--cta">
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://drive.google.com/open?id=0B7jl9f0rvdyPeWZrYk1Zb3RmZDh1Sm0zYWZiYl9PZU1RUDVJ"
-              >
-                Resume
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      this.state.showMobileNav ? renderMobileNav(this.handleShowMobileNav) : renderNavbar(this.handleShowMobileNav)
     )
   }
 }
