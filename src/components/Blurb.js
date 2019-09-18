086a8bd45b4d4cc531404bca83c50854cf32b471
eric-klimowich <eklimowich@hotmail.com>
2019-09-18 11:31:08 -0500

    Testing of jQuery on Blurb component

--- src/components/Blurb.js
+++ src/components/Blurb.js
@@ -1,10 +1,34 @@
-// import React from 'react'
+import React, { Component } from 'react'
+
+import $ from 'jquery'
 
 import { renderBlurbPicLeft, renderBlurbPicRight, renderBlurbPicBoth } from '../Functions'
 
-const Blurb = props => {
-  // console.log(props)
-  return props.blurbImage2 ? renderBlurbPicBoth(props) : props.picLeft ? renderBlurbPicLeft(props) : renderBlurbPicRight(props)
+class Blurb extends Component {
+
+  componentDidMount() {
+    $(".blurb__title--left").fadeOut(2000)
+  }
+
+  render() {
+    return (
+      <div className="blurb">
+        <div className="blurb__info--left">
+          <h1 className="blurb__title--left">
+            {this.props.title}
+          </h1>
+          <p className="blurb__text--left">
+            {this.props.description}
+          </p>
+        </div>
+        <div className="blurb__image--container">
+          <img className="blurb__image" src={this.props.blurbImage} alt={this.props.altImage}/>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Blurb
+
+// return props.blurbImage2 ? renderBlurbPicBoth(props) : props.picLeft ? renderBlurbPicLeft(props) : renderBlurbPicRight(props)
